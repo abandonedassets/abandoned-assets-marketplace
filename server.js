2c4fda16d7e5e6bcadcf71e71a99bb90abbfba02
abandonedassets <info.abandonedassets@gmail.com>
2026-07-09 09:34:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve the static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Fallback to index.html for all routes to maintain the terminal view
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
