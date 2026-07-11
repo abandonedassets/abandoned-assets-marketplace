69474068d3a5c6c1145405304a512f71ca724402
abandonedassets <info.abandonedassets@gmail.com>
2026-07-11 11:45:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Juggernaut Engine Active on port ${PORT}`);
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
